d22c3a6662811e4e457591fc9a9ab9b2ca1e4ca0
Vishnu Dodballapur <vido2373@colorado.edu>
2021-12-06 19:48:46 -0700

    code cleanup image built

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = ab0d0a64a77d1529cae8fadf8268750057386bb1
+AESD_ASSIGNMENTS_VERSION = 25ade68d96713ad85a5f3a7c4d75fd349feea66b
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
